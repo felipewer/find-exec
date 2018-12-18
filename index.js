21ae378ae0b63c08f21b067f20b30db980d0d93f
Homa Wong (unional) <homawong@gmail.com>
2018-12-17 16:15:22 -0800

    original

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-// var exec = require('child_process').execSync
-var exec = require('sync-exec')
-var platform = require('os').platform()
+var exec     = require('sync-exec'),
+    platform = require('os').platform()
 
 module.exports = function(){
-  var commands = Array.isArray(arguments[0]) ? arguments[0] : Array.prototype.slice.apply(arguments)
-  var command = null
+  var self = this
+    , commands = Array.isArray(arguments[0]) ? arguments[0] : Array.prototype.slice.apply(arguments)
+    , command = null
 
   commands.some(function(c){
-    if (exec(findCommand(c)).status === 0){
+    if (exec(findCommand(c)).status == 0){
       command = c
       return true
     }
@@ -17,7 +17,7 @@ module.exports = function(){
 }
 
 function findCommand(command){
-  if (/^win/.test(platform)) {
+  if (/^win/.test(platform)){
     return "where " + command
   } else {
     return "command -v " + command + " >/dev/null 2>&1"
